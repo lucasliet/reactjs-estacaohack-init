50bcbc89a0a9c1d1123e583bb056b73aa9e65a17
Lucas Souza <lucasouliveira@gmail.com>
2020-12-05 13:36:40 +0000

    first component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import OlaMundo from './OlaMundo';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <OlaMundo/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/OlaMundo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OlaMundo() {
+    return (
+        <h1>Olá Mundo!</h1>
+    )
+}
