6c50b487fc20159d7efbfc3236d425f0cad86421
Lucas Souza <lucasouliveira@gmail.com>
2020-12-05 15:17:11 +0000

    componentize dashboard

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 import OlaMundo from './OlaMundo';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <OlaMundo/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home/>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <OlaMundo/>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- src/OlaMundo.jsx
+++ src/OlaMundo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function OlaMundo() {
     const world = 'Mundo';
