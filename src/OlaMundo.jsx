b7910457cb71b1c192bf0d9f47b88296f608a66b
Lucas Souza <lucasouliveira@gmail.com>
2020-12-05 14:10:42 +0000

    print curly braces

--- src/OlaMundo.jsx
+++ src/OlaMundo.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export default function OlaMundo() {
+    const world = 'Mundo';
     return (
-        <h1>Olá Mundo!</h1>
+        <h1>Olá {`{${world}}`}!</h1>
     )
 }
